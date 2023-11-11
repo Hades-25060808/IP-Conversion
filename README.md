8b730034867aac683c81e5e5200bdb3da4099794
Hades <bakytbieku02@gmail.com>
2023-11-11 22:01:47 +0600

    BIG UPDATE
    Fix old function and add new function

--- README.md
+++ README.md
@@ -1,12 +1,26 @@
 # IP-Conversion
- IP conversion app calculator 
-INFORMATION
-  name = "IP-Conversion",
-  version = "0.0.3",
-  author = "Hades",
-  author_email = "bbakytbieku02@gmail.com",
-  description = "IP conversion, conversion DEC in BIN, BIN in DEC, logical multiplication, Automatic network address search.",
-  classifiers = [
-    "Programming Language :: Python :: 3.11',
-  ],
-  "GitHub": "https://github.com/Hades-25060808"
+  IP conversion app calculator 
+
+  Update ver = 0.0.3 ==> ver = 0.0.4
+
+    *add Finder how zero in mask BIN-code
+    *add How many IP addresses this set contains in network
+    *add Finder last ip-address in network
+    *add Finder broadcast ip address
+    
+    *fix Fixing the logical structure in the code
+    *fix Fixed visual code output
+    *fix Fixed the visual part in Readme.md.
+
+  INFORMATION
+
+    name = "IP-Conversion",
+    version = "0.0.4",
+    author = "Hades",
+    author_email = "bbakytbieku02@gmail.com",
+    description = "IP conversion - conversion IP-Address in BIN, subnet mask in BIN, logical multiplication. 
+                   Automatic search - How zero in mask BIN, IP addresses in network, network address, first ip address, Last ip address, Broadcast ip address.",
+    classifiers = [
+      "Programming Language :: Python :: 3.11"
+    ],
+    GitHub: "https://github.com/Hades-25060808"
--- main.py
+++ main.py
@@ -1,34 +1,51 @@
 while True:
     try:
-        def dec_to_bin(ip_dec):
-            result_ip_bin = ".".join(format(int(x), "08b") for x in ip_dec.split("."))
+        def dec_to_bin(ipdeccode):
+            result_ip_bin = ".".join(format(int(x), "08b") for x in ipdeccode.split("."))
             return result_ip_bin
 
-        ip_dec = input("Enter IP-Address for conversion in BIN-conde: ")
+
+        ip_dec = input("$:-Enter IP-Address for conversion in BIN-conde ==> ")
         ip_bin_result = dec_to_bin(ip_dec)
-        print(f"\nYour IP-Address in BIN-code:\n{ip_bin_result}\n{ip_dec}")
+        print(f"---->Your IP-Address in BIN-code:\n        {ip_bin_result} = {ip_dec}")
 
 
-        def subnet_mask_to_bin(mask_to_bin):
-            result_mask_to_bin = ".".join(format(int(x), "08b") for x in mask_to_bin.split("."))
+        def subnet_mask_to_bin(masktobin):
+            result_mask_to_bin = ".".join(format(int(x), "08b") for x in masktobin.split("."))
             return result_mask_to_bin
 
-        mask_to_bin = input("\nEnter Subnet mask: ")
+
+        mask_to_bin = input("$:-Enter Subnet mask ==> ")
         mask_to_bin_result = subnet_mask_to_bin(mask_to_bin)
-        print(f"\nYour subnet mask in BIN-code:\n{mask_to_bin_result}\n{mask_to_bin}")
+        print(f"---->Your subnet mask in BIN-code:\n        {mask_to_bin_result} = {mask_to_bin}")
 
 
-        def logical_and_with_dots(bin_str1, bin_str2):
-            parts1 = bin_str1.split('.')
-            parts2 = bin_str2.split('.')
+        def count_zeros(BINstr):
+            count = BINstr.count('0')
+            return count
+
+
+        bin_str = mask_to_bin_result
+        zero_count = count_zeros(bin_str)
+        print(f"------------>How zero in mask BIN-code:\n                {zero_count}")
+
+        math = zero_count
+        math1 = 2 ** zero_count
+        math2 = math1 - 2
+        print(f"-------------------->IP addresses in network:\n                        {math2}")
+
+
+        def logical_and_with_dots(BINstr1, BINstr2):
+            parts1 = BINstr1.split('.')
+            parts2 = BINstr2.split('.')
 
             if len(parts1) != len(parts2):
-                raise ValueError("Длины строк не совпадают")
+                raise ValueError("!!!The number of octets does not match!!!")
 
             result_parts = []
             for part1, part2 in zip(parts1, parts2):
                 if len(part1) != len(part2):
-                    raise ValueError("Длины строк не совпадают")
+                    raise ValueError("!!!String lengths do not match!!!")
 
                 result_part = ""
                 for bit1, bit2 in zip(part1, part2):
@@ -38,35 +55,63 @@ while True:
                         result_part += '0'
                 result_parts.append(result_part)
 
-            result = '.'.join(result_parts)
-            return result
+            result0 = '.'.join(result_parts)
+            return result0
+
 
         bin_str1 = ip_bin_result
         bin_str2 = mask_to_bin_result
         result = logical_and_with_dots(bin_str1, bin_str2)
-        print(f"\nResult of logical multiplication:\n{result}\n")
+        print(f"---------------------------->Result of logical multiplication:\n                                {result}")
 
 
-        def bin_to_dec(ip_bin):
-            result_ip_dec = ".".join(str(int(x, 2)) for x in ip_bin.split("."))
+        def bin_to_dec(ipbin):
+            result_ip_dec = ".".join(str(int(x, 2)) for x in ipbin.split("."))
             return result_ip_dec
+
 
         ip_bin = result
         ip_dec_result = bin_to_dec(ip_bin)
-        print(f"It's Your Network address:\n{ip_dec_result}\n")
+        print(f"---------------------------->It's Your Network address:\n                        {ip_dec_result}")
 
 
-        def increment_last_digit(ip_dec):
-            octets = ip_dec.split('.')
+        def increment_last_digit(ipdec):
+            octets = ipdec.split('.')
             last_octet = int(octets[-1]) + 1
             octets[-1] = str(last_octet)
             result_ip = '.'.join(octets)
             return result_ip
 
-        ip_dec = ip_dec_result
-        new_ip = increment_last_digit(ip_dec)
-        print(f"First address in network:\n{new_ip}\n")
 
+        first_ip_address = ip_dec_result
+        new_ip = increment_last_digit(first_ip_address)
+        print(f"-------------------->First address in network:\n                {new_ip}")
+
+
+        def last_ip_address(lastip):
+            octets = lastip.split('.')
+            last_octet = int(octets[-1]) + math2
+            octets[-1] = str(last_octet)
+            result_ip = '.'.join(octets)
+            return result_ip
+
+
+        last_ip = ip_dec_result
+        last_ip_in_network = last_ip_address(last_ip)
+        print(f"------------>Last ip address:\n        {last_ip_in_network}")
+
+
+        def broadcast_ip_address(lastip):
+            octets = lastip.split('.')
+            last_octet = int(octets[-1]) + math2 + 1
+            octets[-1] = str(last_octet)
+            result_ip = '.'.join(octets)
+            return result_ip
+
+
+        broadcast = ip_dec_result
+        broadcast_ip_address_in_network = broadcast_ip_address(broadcast)
+        print(f"---->Broadcast ip address:\n{broadcast_ip_address_in_network}\n")
 
     except ValueError:
         print("ERROR!!! TRY AGAIN")
